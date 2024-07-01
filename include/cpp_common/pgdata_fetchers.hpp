858d0a37c30ab6ce8d9b226c4130c2095fff4869
cvvergara <vicky@erosion.dev>
2024-07-01 11:25:29 -0600

    (include/cpp_common/pgdata_fetchers.hpp) Fetchers grouping

---
+++ include/cpp_common/pgdata_fetchers.hpp
@@ -0,0 +1,81 @@
+/*PGR-GNU*****************************************************************
+File: pgdata_fetchers.hpp
+
+Copyright (c) 2015 pgRouting developers
+Mail: project@pgrouting.org
+
+Developers:
+Copyright (c) 2015 Celia Virginia Vergara Castillo
+Mail: vicky at erosion.dev
+
+Copyright (c) 2021 Ashish Kumar
+Mail: ashishkr23438 at gmail.com
+
+----
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ ********************************************************************PGR-GNU*/
+
+#ifndef INCLUDE_CPP_COMMON_PGDATA_FETCHERS_HPP_
+#define INCLUDE_CPP_COMMON_PGDATA_FETCHERS_HPP_
+#pragma once
+
+/* for HeapTuple, TupleDesc */
+extern "C" {
+#include <postgres.h>
+#include <access/htup_details.h>
+}
+
+#include <vector>
+
+class Info;
+class Vroom_matrix_t;
+class Vroom_time_window_t;
+class Vroom_job_t;
+class Vroom_break_t;
+class Vroom_shipment_t;
+class Vroom_vehicle_t;
+class Matrix_cell_t;
+class PickDeliveryOrders_t;
+class Vehicle_t;
+class Time_multipliers_t;
+
+namespace vrprouting {
+namespace pgget {
+
+
+namespace pickdeliver {
+
+Matrix_cell_t fetch_matrix(const HeapTuple, const TupleDesc&, const std::vector<Info>&, bool);
+PickDeliveryOrders_t fetch_orders(const HeapTuple, const TupleDesc&, const std::vector<Info>&, bool);
+Time_multipliers_t fetch_timeMultipliers(const HeapTuple, const TupleDesc&, const std::vector<Info>&, bool);
+Vehicle_t fetch_vehicles(const HeapTuple, const TupleDesc&, const std::vector<Info>&, bool);
+
+}  // namespace pickdeliver
+
+namespace vroom {
+Vroom_matrix_t fetch_matrix(const HeapTuple, const TupleDesc&, const std::vector<Info>&, bool);
+Vroom_time_window_t fetch_timewindows(const HeapTuple, const TupleDesc&, const std::vector<Info>&, bool);
+Vroom_job_t fetch_jobs(const HeapTuple, const TupleDesc&, const std::vector<Info>&, bool);
+Vroom_break_t fetch_breaks(const HeapTuple, const TupleDesc&, const std::vector<Info>&, bool);
+Vroom_shipment_t fetch_shipments(const HeapTuple, const TupleDesc&, const std::vector<Info>&, bool);
+Vroom_vehicle_t fetch_vehicles(const HeapTuple, const TupleDesc&, const std::vector<Info>&, bool);
+}  // namespace vroom
+
+}  // namespace pgget
+}  // namespace vrprouting
+
+#endif  // INCLUDE_C_COMMON_PGDATA_FETCHERS_H_
