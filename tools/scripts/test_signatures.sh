684295ec495170bbdb50f80741fccec174e75319
cvvergara <vicky@erosion.dev>
2024-07-01 17:01:24 -0600

    (test_signatures.sh) Updating file

--- tools/scripts/test_signatures.sh
+++ tools/scripts/test_signatures.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
-
+# /*PGR-GNU*****************************************************************
+# File: test_signatures.sh
+# Copyright (c) 2020 pgRouting developers
+# Mail: project@pgrouting.org
+# ------
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# ********************************************************************PGR-GNU*/
 # This test checks that signatures within mayors
 # Call from the root of the repository
 
 DIR=$(git rev-parse --show-toplevel)/sql/sigs
 
 pushd "${DIR}" > /dev/null || exit
-SIGNATURES=$(git ls-files *.sig | perl -pe 's/vrprouting--(.*)\.sig/$1/')
-for s1 in ${SIGNATURES[@]}
+# For bash, uses temporary files
+mapfile -t SIGNATURES < <(git ls-files "*.sig" | perl -pe 's/vrprouting--(.*)\.sig/$1/')
+
+for s1 in "${SIGNATURES[@]}"
 do
-    for s2 in ${SIGNATURES[@]}
+    for s2 in "${SIGNATURES[@]}"
     do
         # only comparing lower version with higher version
         if (( $(echo "$s1 >= $s2" | bc -l) )); then continue; fi
