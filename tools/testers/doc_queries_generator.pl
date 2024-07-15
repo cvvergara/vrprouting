9531da8162c4840c58d7a1a6bbe98aa8f690f459
cvvergara <vicky@erosion.dev>
2024-07-15 10:03:20 -0600

    Processing or_tools data

--- tools/testers/doc_queries_generator.pl
+++ tools/testers/doc_queries_generator.pl
@@ -254,6 +254,7 @@ sub run_test {
     mysystem("$psql $connopts -A -t -q -f tools/testers/matrix_new_values.sql $DBNAME >> $TMP2 2>\&1 ");
     mysystem("$psql $connopts -A -t -q -f tools/testers/functions.sql $DBNAME >> $TMP2 2>\&1 ");
     mysystem("$psql $connopts -A -t -q -f tools/testers/solomon_100_rc101.data.sql $DBNAME >> $TMP2 2>\&1 ");
+    mysystem("$psql $connopts -A -t -q -f tools/testers/ortools.data.sql $DBNAME >> $TMP2 2>\&1 ");
 
     # process data for the tests (if any)
     for my $data_file (@{$t->{data}}) {
