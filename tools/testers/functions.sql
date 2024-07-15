5cb13329d63ff4eeb0b6068436230db75ae87509
cvvergara <vicky@erosion.dev>
2024-07-15 11:46:52 -0600

    data functions and ortools on tools

--- tools/testers/functions.sql
+++ tools/testers/functions.sql
@@ -33,7 +33,9 @@ END;
 $BODY$
 LANGUAGE plpgsql;
 
-/* for compatible vehicles */
+/**
+For testing compatability
+**/
 DROP FUNCTION IF EXISTS test_all_shipments;
 CREATE OR REPLACE FUNCTION test_all_shipments()
 RETURNS VOID AS
@@ -61,6 +63,111 @@ BEGIN
 
   RAISE NOTICE 'order % Compatible vehicles %', order_row.id, info.v ;
   END LOOP;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+
+DROP FUNCTION IF EXISTS test_all_shipments_raw;
+CREATE OR REPLACE FUNCTION test_all_shipments_raw()
+RETURNS VOID AS
+$BODY$
+DECLARE
+order_row RECORD;
+info RECORD;
+
+BEGIN
+  FOR order_row IN SELECT * FROM shipments ORDER BY booking_date LOOP
+    SELECT array_agg(vehicle_id ORDER BY vehicle_id) AS v
+    FROM vrp_compatibleVehiclesRaw(
+      $$SELECT * FROM shipments$$,
+      $$SELECT * FROM vehicles$$::TEXT,
+      $$SELECT start_vid, end_vid, agg_cost FROM timematrix$$,
+      $$SELECT * FROM tdm_raw('2019-12-09'::TIMESTAMP, '2019-12-13'::TIMESTAMP)$$,
+      order_row.id, factor=>1.0::FLOAT
+  ) INTO info;
+
+  RAISE NOTICE 'order % Compatible vehicles %', order_row.id, info.v ;
+  END LOOP;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+/* testing view route not used currently */
+DROP FUNCTION IF EXISTS test_all_vehicles;
+CREATE OR REPLACE FUNCTION test_all_vehicles()
+RETURNS VOID AS
+$BODY$
+DECLARE
+vehicle_row RECORD;
+info RECORD;
+
+BEGIN
+    FOR vehicle_row IN SELECT * FROM vehicles ORDER BY id LOOP
+        RAISE NOTICE 'vehicle %:', vehicle_row.id;
+        FOR info IN
+            SELECT *
+            FROM vrp_viewRoute(
+              $$SELECT * FROM shipments$$::TEXT,
+              $$SELECT * FROM vehicles WHERE stops IS NOT NULL$$::TEXT,
+              $$SELECT start_vid, end_vid, agg_cost *  3600 AS agg_cost FROM timeMatrix$$::TEXT,
+              $$SELECT * FROM tdm('2019-12-09'::TIMESTAMP, '2019-12-13'::TIMESTAMP)$$::TEXT,
+              v_id => vehicle_row.id, factor => 1.0::float
+            ) ORDER BY stop_seq
+        LOOP
+
+            RAISE NOTICE ' vehicle %:  (%)  type=%  stop_id=% o=% c=% tt=%  a=%  wt=%  s=%  st=% d=%',
+                info.vehicle_id, info.stop_seq, info.stop_type,
+                info.stop_id,
+                info.shipment_id,
+                info.load,
+                info.travel,
+                info.arrival,
+                info.waiting_time,
+                info.schedule,
+                info.service,
+                info.departure;
+        END LOOP;
+    END LOOP;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS test_all_vehicles_raw;
+CREATE OR REPLACE FUNCTION test_all_vehicles_raw()
+RETURNS VOID AS
+$BODY$
+DECLARE
+vehicle_row RECORD;
+info RECORD;
+
+BEGIN
+    FOR vehicle_row IN SELECT * FROM vehicles ORDER BY id LOOP
+        RAISE NOTICE 'vehicle %:', vehicle_row.id;
+        FOR info IN
+            SELECT *
+            FROM vrp_viewRouteRaw(
+              $$SELECT * FROM shipments$$::TEXT,
+              $$SELECT * FROM vehicles WHERE stops IS NOT NULL$$::TEXT,
+              $$SELECT start_vid, end_vid, agg_cost *  3600 AS agg_cost FROM timeMatrix$$::TEXT,
+              $$SELECT * FROM tdm_raw('2019-12-09'::TIMESTAMP, '2019-12-13'::TIMESTAMP)$$::TEXT,
+              v_id => vehicle_row.id, factor => 1.0::float
+            ) ORDER BY stop_seq
+        LOOP
+
+            RAISE NOTICE ' vehicle %:  (%)  type=%  stop_id=% o=% c=% tt=%  a=%  wt=%  s=%  st=% d=%',
+                info.vehicle_id, info.stop_seq, info.stop_type,
+                info.stop_id,
+                info.shipment_id,
+                info.load,
+                info.travel_fd,
+                info.arrival_ft,
+                info.wait_fd,
+                info.schedule_ft,
+                info.service_fd,
+                info.departure_ft;
+        END LOOP;
+    END LOOP;
 END;
 $BODY$
 LANGUAGE plpgsql;
