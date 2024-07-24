33f5e3f7779f860f1249ebdb3eaad3f4e90e55c5
cvvergara <vicky@erosion.dev>
2024-07-24 13:52:46 -0600

    Using original files

--- include/c_types/vroom_rt.h
+++ include/c_types/vroom_rt.h
@@ -1,12 +1,12 @@
 /*PGR-GNU*****************************************************************
-File: compatibleVehicles_rt.h
+File: vroom_rt.h
 
 Copyright (c) 2021 pgRouting developers
 Mail: project@pgrouting.org
 
-Developer:
-Copyright (c) 2021 Celia Virginia Vergara Castillo
-Copyright (c) 2021 Joseph Emile Honour Percival
+Function's developer:
+Copyright (c) 2021 Ashish Kumar
+Mail: ashishkr23438@gmail.com
 
 ------
 
@@ -27,77 +27,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  ********************************************************************PGR-GNU*/
 /*! @file */
 
-#ifndef INCLUDE_C_TYPES_RETURN_TYPES_H_
-#define INCLUDE_C_TYPES_RETURN_TYPES_H_
+#ifndef INCLUDE_C_TYPES_VROOM_VROOM_RT_H_
+#define INCLUDE_C_TYPES_VROOM_VROOM_RT_H_
 #pragma once
 
 #include "c_types/typedefs.h"
 
-/** @brief order-vehicle compatability relationship
-
-  @note C/C++/postgreSQL connecting structure for output
-  name | description
-  :----- | :-------
-  order_id | Order's identifier
-  vehicle_id | Vehicle's identifier
-  */
-
-struct CompatibleVehicles_rt {
-  int64_t order_id;    /** order's identifier */
-  int64_t vehicle_id;  /** vehicle's identifier */
-};
-
-/** @brief short_vehicle
-
-@note C/C++/postgreSQL connecting structure for input
-name | description
-:----- | :-------
-id | Vehicle's identifier
-stops | Vehicle's stops
-*/
-struct Short_vehicle_rt{
-  Id  vehicle_id; /** Vehicle's identifier */
-  Id  order_id;   /** Stop */
-};
-
-/** @brief Solution schedule when twv & cw are hard restrictions
-
-@note C/C++/postgreSQL connecting structure for output
-name | description
-:----- | :-------
-vehicle_seq | Sequence for ordering for a single vehicle
-vehicle_id | Vehicle's identifier
-stop_seq | Stop sequence of vehicle
-order_id | Node identifier of the stop
-stop_type | Kind of stop
-cargo | Cargo when leaving the stop
-travelDuration | Travel time from previous stop
-arrivalTime | Vehicle's arrival time
-waitDuration | Vehicle's wait time before stop opens
-operationTime | Time at which picked up or droped off takes place
-serviceDuration | Service time of the stop
-departureTime | Vehicle's departure time from the stop
-*/
-
-struct Solution_rt {
-    int vehicle_seq;         /** Sequence for ordering for a single vehicle */
-    int64_t vehicle_id;      /** Vehicle's identifier */
-    int stop_seq;            /** Stop sequence of vehicle */
-    int64_t order_id;        /** Served order's identifier */
-    int64_t stop_id;         /** Node identifier of the stop */
-    int stop_type;           /** Kind of stop */
-    int64_t cargo;            /** Cargo when leaving the stop */
-    int64_t travelTime;       /** Travel time from previous stop */
-    int64_t arrivalTime;      /** Vehicle's arrival time */
-    int64_t waitDuration;     /** Vehicle's wait time before stop opens */
-    int64_t operationTime;    /** Time at which the pickup or delivery takes place */
-    int64_t serviceDuration;  /** Service time of the stop */
-    int64_t departureTime;    /** Vehicle's departure time from the stop */
-    int cvTot;               /** Total Capacity Violations */
-    int twvTot;              /** Total Time Window Violations */
-};
-
-/** @brief VROOM return type
+/** @brief Solution's attributes
 
 @note C/C++/postgreSQL connecting structure for input
 name | description
@@ -140,4 +76,4 @@ struct Vroom_rt {
   size_t load_size;        /** Vehicle's load array size */
 };
 
-#endif  // INCLUDE_C_TYPES_RETURN_TYPES_H_
+#endif  // INCLUDE_C_TYPES_VROOM_VROOM_RT_H_
