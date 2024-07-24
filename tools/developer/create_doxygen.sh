02f2c148eda47c086adeceec106a7386e7394376
cvvergara <vicky@erosion.dev>
2024-07-24 09:12:03 -0600

    matching tool to main

--- tools/developer/create_doxygen.sh
+++ tools/developer/create_doxygen.sh
@@ -7,5 +7,6 @@ make doxy
 cd build || exit
 echo "INFO: documentation is in build/doxygen/html"
 echo "INFO: to build the pdf run the following commands:"
-echo "cd build/doxygen/latex pdflatex refman.tex"
+echo "cd build/doxygen/latex
+pdflatex refman.tex"
 echo "INFO: this requires tex to be installed"
