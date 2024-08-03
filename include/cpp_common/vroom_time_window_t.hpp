17b715812a93a12e905df1e9c3c2caac077825a5
cvvergara <vicky@erosion.dev>
2024-08-03 12:24:46 -0600

    removing empty lines

--- include/cpp_common/vroom_time_window_t.hpp
+++ include/cpp_common/vroom_time_window_t.hpp
@@ -35,8 +35,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 namespace vrprouting {
 
-
-
 /** @brief Time window's attributes
 
   @note C/C++/postgreSQL connecting classure for input
