03d3c2cb179c66f848931b26d5e929fc8c824e20
cvvergara <vicky@erosion.dev>
2025-07-25 12:21:48 -0600

    (tools/testers) Adding files for docqueries and pgtap executions

---
+++ tools/testers/setup_db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# /*PGR-GNU*****************************************************************
+#
+# License: GNU General Public License v2.0
+# Copyright (c) 2025 pgORpy developers
+# Mail: project@pgrouting.org
+#
+#  ********************************************************************PGR-GNU*/
+
+set -e
+
+DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+PGOPTIONS='-c client_min_messages=WARNING' psql -p "$1" -U "$3"  -d "$2" -X -q --set ON_ERROR_STOP=1 --pset pager=off \
+    -c "CREATE EXTENSION IF NOT EXISTS pgtap; CREATE EXTENSION IF NOT EXISTS pgorpy WITH VERSION '${4}' CASCADE;"
+
+PGOPTIONS='-c client_min_messages=WARNING' psql -p "$1" -U "$3"  -d "$2" -X -q --set ON_ERROR_STOP=1 --pset pager=off \
+    -f "${DIR}/sampledata.sql" \
+    -f "${DIR}/general_pgtap_tests.sql" \
+    -f "${DIR}/no_crash_test.sql"
