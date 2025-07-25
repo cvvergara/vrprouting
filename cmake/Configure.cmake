b045822b460c3f83947843d48840bfa52731e7f3
cvvergara <vicky@erosion.dev>
2025-07-24 21:02:42 -0600

    Cleaning the build as it does not use C/C++

--- cmake/Configure.cmake
+++ cmake/Configure.cmake
@@ -4,25 +4,22 @@
 #---------------------------------------------
 #
 # Name of the directories that have
-# - C/C++ code
 # - SQL code
 # - documentation code
 #
 # structure:
 #
-# directory | code | sql | doc
+# directory | sql | doc
 #
 # where:
 #
 # directory: is the name of the directory
-# code: Y / N value, when "Y" C/C++ code will be looked for
 # sql: Y / N value, when "Y" SQL code will be looked for
 # doc: Y / N value, when "Y" Documentation code will be looked for
 #----------------------
 configure_file("configuration.conf" "configuration.conf")
 file(STRINGS configuration.conf PROJECT_CONFIGURATION_FILE)
 
-set(PROJECT_SRC_DIRECTORIES "")
 set(PROJECT_SQL_DIRECTORIES "")
 set(PROJECT_DOC_DIRECTORIES "")
 foreach(line ${PROJECT_CONFIGURATION_FILE})
@@ -30,20 +27,14 @@ foreach(line ${PROJECT_CONFIGURATION_FILE})
     if("${comment}" MATCHES "#")
         continue()
     endif()
-    string(REGEX REPLACE "^(.*)\\|(.*)\\|(.*)\\|(.*)" "\\1" directory ${line})
-    string(REGEX REPLACE "^(.*)\\|(.*)\\|(.*)\\|(.*)" "\\2" has_code ${line})
-    string(REGEX REPLACE "^(.*)\\|(.*)\\|(.*)\\|(.*)" "\\3" has_sql ${line})
-    string(REGEX REPLACE "^(.*)\\|(.*)\\|(.*)\\|(.*)" "\\4" has_doc ${line})
+    string(REGEX REPLACE "^(.*)\\|(.*)\\|(.*)" "\\1" directory ${line})
+    string(REGEX REPLACE "^(.*)\\|(.*)\\|(.*)" "\\2" has_sql ${line})
+    string(REGEX REPLACE "^(.*)\\|(.*)\\|(.*)" "\\3" has_doc ${line})
 
     string(STRIP ${directory} directory)
-    string(STRIP ${has_code} has_code)
     string(STRIP ${has_sql} has_sql)
     string(STRIP ${has_doc} has_doc)
 
-
-    if( ${has_code} MATCHES "Y")
-        list(APPEND PROJECT_SRC_DIRECTORIES "${directory}")
-    endif()
     if( ${has_sql} MATCHES "Y")
         list(APPEND PROJECT_SQL_DIRECTORIES "${directory}")
     endif()
@@ -53,7 +44,6 @@ foreach(line ${PROJECT_CONFIGURATION_FILE})
 endforeach()
 
 if (PROJECT_DEBUG)
-    message(STATUS "PROJECT_SRC_DIRECTORIES ${PROJECT_SRC_DIRECTORIES}")
     message(STATUS "PROJECT_SQL_DIRECTORIES ${PROJECT_SQL_DIRECTORIES}")
     message(STATUS "PROJECT_DOC_DIRECTORIES ${PROJECT_DOC_DIRECTORIES}")
 endif()
