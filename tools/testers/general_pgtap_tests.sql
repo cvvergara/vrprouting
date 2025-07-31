48f6714723d78d4135631e2552742091a3508c60
cvvergara <vicky@erosion.dev>
2025-07-31 16:06:49 -0600

    some more similarities

--- tools/testers/general_pgtap_tests.sql
+++ tools/testers/general_pgtap_tests.sql
@@ -1,11 +1,30 @@
+/*PGR-GNU*****************************************************************
+
+Copyright (c) 2025  pgRouting developers
+Mail: project@pgrouting.org
+
+------
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ********************************************************************PGR-GNU*/
+
 -- checks the minimum version version for C++ code
-CREATE OR REPLACE FUNCTION public.min_lib_version(min_version TEXT)
+CREATE OR REPLACE FUNCTION min_lib_version(min_version TEXT)
 RETURNS BOOLEAN AS
 $BODY$
 DECLARE val BOOLEAN;
 BEGIN
 WITH
-  current_version AS (SELECT string_to_array(regexp_replace((SELECT library FROM vrp_full_version()), '.*-', '', 'g'),'.')::int[] AS version),
+  current_version AS (SELECT string_to_array(regexp_replace((SELECT library FROM pgr_full_version()), '.*-', '', 'g'),'.')::int[] AS version),
   asked_version AS (SELECT string_to_array(min_version, '.')::int[] AS version)
   SELECT (current_version.version >= asked_version.version) FROM current_version, asked_version INTO val;
   RETURN val;
@@ -14,16 +33,64 @@ $BODY$
 LANGUAGE plpgsql;
 
 -- checks the minimum version version for SQL code
-CREATE OR REPLACE FUNCTION public.min_version(min_version TEXT)
+CREATE OR REPLACE FUNCTION min_version(min_version TEXT)
 RETURNS BOOLEAN AS
 $BODY$
 DECLARE val BOOLEAN;
 BEGIN
 WITH
-  current_version AS (SELECT string_to_array(regexp_replace(vrp_version(), '-.*', '', 'g'),'.')::int[] AS version),
+  current_version AS (SELECT string_to_array(regexp_replace(pgr_version(), '-.*', '', 'g'),'.')::int[] AS version),
   asked_version AS (SELECT string_to_array(min_version, '.')::int[] AS version)
   SELECT (current_version.version >= asked_version.version) FROM current_version, asked_version INTO val;
   RETURN val;
 END;
 $BODY$
 LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION function_types_eq(TEXT, TEXT)
+RETURNS TEXT AS
+$BODY$
+    SELECT set_eq(format($$
+      WITH
+      a AS (
+        SELECT oid, u.name, u.idx
+        FROM pg_catalog.pg_proc p CROSS JOIN unnest(coalesce(p.proallargtypes, p.proargtypes))
+        WITH ordinality as u(name, idx)
+        WHERE p.proname = '%1$s'),
+      b AS (
+        SELECT a.*, t.typname FROM a JOIN pg_catalog.pg_type As t on (t.oid = a.name))
+      SELECT array_agg(typname ORDER BY idx)  FROM b GROUP BY oid
+      $$, $1), $2, $1 || ': Function types');
+$BODY$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION function_types_has(TEXT, TEXT)
+RETURNS TEXT AS
+$BODY$
+    SELECT set_has(format($$
+      WITH
+      a AS (
+        SELECT oid, u.name, u.idx
+        FROM pg_proc p CROSS JOIN unnest(p.proallargtypes)
+        WITH ordinality as u(name, idx)
+        WHERE p.proname = '%1$s'),
+      b AS (
+        SELECT a.*, t.typname FROM a JOIN pg_type As t on (t.oid = a.name))
+      SELECT array_agg(typname ORDER BY idx)  FROM b GROUP BY oid
+      $$, $1), $2, $1 || ': Function types');
+$BODY$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION function_args_eq(TEXT, TEXT)
+RETURNS TEXT AS
+$BODY$
+    SELECT set_eq(format($$
+      SELECT proargnames from pg_catalog.pg_proc where proname = '%1$s'
+      $$, $1), $2, $1 || ': Function args names');
+$BODY$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION function_args_has(TEXT, TEXT)
+RETURNS TEXT AS
+$BODY$
+    SELECT set_has(format($$
+      SELECT proargnames from pg_catalog.pg_proc where proname = '%1$s'
+      $$, $1), $2, $1 || ': Function has args names');
+$BODY$ LANGUAGE SQL;
---
+++ tools/testers/setup_db.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# PGR-GNU*****************************************************************
+# File: setup_db.sh
+# Copyright (c) 2025 pgRouting developers
+# Mail: project@pgrouting.org
+# ------
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# ********************************************************************PGR-GNU*/
+set -e
+
+DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+PGOPTIONS='-c client_min_messages=WARNING' psql -p "$1" -U "$3"  -d "$2" -X -q --set ON_ERROR_STOP=1 --pset pager=off \
+    -c "CREATE EXTENSION IF NOT EXISTS pgtap; CREATE EXTENSION IF NOT EXISTS pgorpy WITH VERSION '${4}' CASCADE;"
+
+PGOPTIONS='-c client_min_messages=WARNING' psql -p "$1" -U "$3"  -d "$2" -X -q --set ON_ERROR_STOP=1 --pset pager=off \
+    -f "${DIR}/sampledata.sql" \
+    -f "${DIR}/general_pgtap_tests.sql"
