e8c881b0b8067fd695c7325828c54d4f6fd3e4a3
cvvergara <vicky@erosion.dev>
2025-08-06 15:55:12 -0600

    Adding sample data for ortools

--- tools/testers/sampledata.sql
+++ tools/testers/sampledata.sql
@@ -18,3 +18,48 @@ $BODY$
 $BODY$;
 
 -- activate_python_end
+
+CREATE TABLE bin_packing(
+  id INTEGER,
+  weight INTEGER);
+
+INSERT INTO bin_packing(id, weight)
+VALUES
+(1, 48), (2, 30), (3, 19), (4, 36), (5, 36), (6, 27), (7, 42), (8, 42), (9, 36), (10, 24), (11, 30);
+
+
+CREATE TABLE knapsack(
+  id INTEGER,
+  weight INTEGER,
+  cost INTEGER);
+
+INSERT INTO knapsack(id, weight, cost)
+VALUES
+(1, 12, 4),
+(2, 2, 2),
+(3, 1, 1),
+(4, 4, 10),
+(5, 1, 2);
+
+CREATE TABLE multiple_knapsack(
+  id INTEGER,
+  weight INTEGER,
+  cost INTEGER);
+
+INSERT INTO multiple_knapsack(id, weight, cost)
+VALUES
+(1, 48, 10),
+(2, 30, 30),
+(3, 42, 25),
+(4, 36, 50),
+(5, 36, 35),
+(6, 48, 30),
+(7, 42, 15),
+(8, 42, 40),
+(9, 36, 30),
+(10, 24, 35),
+(11, 30, 45),
+(12, 30, 10),
+(13, 42, 20),
+(14, 36, 30),
+(15, 36, 25);
